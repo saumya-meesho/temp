d4ee39e854d0ae9d1d40bd43caaaf968655f955a
saumya tiwari <saumya.tiwari@meesho.com>
2025-07-15 14:40:14 +0530

    Create V1_create_table_and_alter.sql
    qwerty

---
+++ MYSQL/meesho-admin-dev-0622/ab/V1_create_table_and_alter.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `audience_log` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `audience_id` int NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `description` text NOT NULL,
+  `type` varchar(32) DEFAULT NULL,
+  `refresh_query` text,
+  `created_timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `aud_id` (`audience_id`),
+  CONSTRAINT `audience_change_log_ibfk_1` FOREIGN KEY (`audience_id`) REFERENCES `audiences` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=26696 DEFAULT CHARSET=latin1;
+
+ALTER TABLE audiences ADD COLUMN age INT;
