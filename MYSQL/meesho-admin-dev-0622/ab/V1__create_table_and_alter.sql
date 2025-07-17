68530a34524034769a26b109331eee7c68e9d1bc
saumya tiwari <saumya.tiwari@meesho.com>
2025-07-17 14:28:59 +0530

    Update V1__create_table_and_alter.sql

--- MYSQL/meesho-admin-dev-0622/ab/V1__create_table_and_alter.sql
+++ MYSQL/meesho-admin-dev-0622/ab/V1__create_table_and_alter.sql
@@ -11,4 +11,18 @@ CREATE TABLE `audience_log` (
   CONSTRAINT `audience_log_ibfk_1` FOREIGN KEY (`audience_id`) REFERENCES `audiences` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE=InnoDB AUTO_INCREMENT=26696 DEFAULT CHARSET=latin1;
 
+Create Table: CREATE TABLE `audience_renewal` (
+  `audience_id` int NOT NULL,
+  `renewal_status` enum('STOPPED','RENEWED','EXPIRED','PENDING') NOT NULL,
+  `expiry_date_timestamp` timestamp NOT NULL,
+  `renewed_by` varchar(64) NOT NULL,
+  `renewed_count` int NOT NULL,
+  `watchers_list` varchar(512) NOT NULL,
+  `renewal_freq_days` int NOT NULL,
+  `created_timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`audience_id`),
+  CONSTRAINT `audience_renewal_ibfk_1` FOREIGN KEY (`audience_id`) REFERENCES `audiences` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  
 ALTER TABLE audiences ADD COLUMN created_timestamp timestamp NULL DEFAULT CURRENT_TIMESTAMP;
